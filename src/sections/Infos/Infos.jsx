771e8e6873df4817fa8a91f51ef5f1e90b7166d6
thaislefinesseur <150953959+thaislefinesseur@users.noreply.github.com>
2023-12-01 15:10:02 +0100

    Update Infos.jsx

--- src/sections/Infos/Infos.jsx
+++ src/sections/Infos/Infos.jsx
@@ -17,10 +17,10 @@ function Infos() {
                     <div className="left-infos">
                         <h1>Measurements</h1>
                         <p className="fst">height : 188cm / 6.2ft</p>
-                        <p>weight : 73kg / 162 lbs</p>
-                        <p>chest : 93cm</p>
-                        <p>waist : 74cm</p>
-                        <p>hips : 93cm</p>
+                        <p>weight : 70kg / 154lbs</p>
+                        <p>chest : 90cm</p>
+                        <p>waist : 73cm</p>
+                        <p>hips : 92cm</p>
                         <p className="lst">shoe size : 43FR</p>
                     </div>
                     <div className="right-infos">
